a0647dc6b350f739075d4257ccd0e077f8749159
chuiizeet <chuy.guerra19@outlook.es>
2019-08-13 22:52:00 -0500

    Add Recicler List

--- app/src/main/java/com/jegulabs/recicler/MainActivity.java
+++ app/src/main/java/com/jegulabs/recicler/MainActivity.java
@@ -3,11 +3,18 @@ package com.jegulabs.recicler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.jegulabs.recicler.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements RestaurantFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
